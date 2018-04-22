eeb402afb1b3369bc42fd6cb22b1d6d67a9ea703
SebastianAlba <sebasjosue84@gmail.com>
2018-04-22 14:50:05 -0600

    Definición de clases
    Se implementaron métodos en las clases abb y main.

--- src/main.java
+++ src/main.java
@@ -1,4 +1,25 @@
 
 public class main {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		abb arbol = new abb ();
+	    arbol.insertar (100);
+	    arbol.insertar (10);
+	    arbol.insertar (17);
+	    arbol.insertar (200);
+	    arbol.insertar (11);
+	    arbol.insertar (7);
+	    arbol.eliminar(10);
+	    arbol.eliminar(7);
+
+
+	    System.out.print ("Impresion preOrden: ");
+	    arbol.preOrder();
+	    System.out.print ("Impresion inOrden: ");
+	    arbol.inOrder();
+	    System.out.print ("Impresion posOrden: ");
+	    arbol.posOrder();
+
+}
 }
